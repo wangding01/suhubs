391f2f3b61ffab5ac95c29c78491248899cbff64
ipop <936590874@qq.com>
2023-04-18 16:26:32 +0800

    update

--- docs/js/lib/docsify.js
+++ docs/js/lib/docsify.js
@@ -1,21 +1,20 @@
-
 function docsifyBuilder() {
     window.$docsify = {
-        name: '一个实例',
+        name: 'PDD',
         repo: 'https://hongxiangwang.github.io/docsify-demo/',
         plugins: [
-            function (hook, vm) {
-                hook.init(function () {
+            function(hook, vm) {
+                hook.init(function() {
                     // 初始化完成后调用，只调用一次，没有参数。
                 });
 
-                hook.beforeEach(function (content) {
+                hook.beforeEach(function(content) {
                     // 每次开始解析 Markdown 内容时调用
                     // ...
                     return content;
                 });
 
-                hook.afterEach(function (html, next) {
+                hook.afterEach(function(html, next) {
                     // 解析成 html 后调用。
                     // beforeEach 和 afterEach 支持处理异步逻辑
                     // ...
@@ -27,22 +26,23 @@ function docsifyBuilder() {
 
                 });
 
-                hook.doneEach(function () {
+                hook.doneEach(function() {
                     // 每次路由切换时数据全部加载完成后调用，没有参数。
                     // ...
                 });
 
-                hook.mounted(function () {
+                hook.mounted(function() {
                     // 初始化并第一次加载完成数据后调用，只触发一次，没有参数。
                 });
 
-                hook.ready(function () {
+                hook.ready(function() {
                     // 初始化并第一次加载完成数据后调用，没有参数。
                 });
             }
         ],
 
-        
+        loadSidebar: true
+
     }
 
 }
